128ad4b51c831202d7c2daa410b7fc1496b0e705
brankop87 <51298519+brankop87@users.noreply.github.com>
2025-11-10 02:01:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export const metadata = {
   title: "Petković Solutions",
@@ -12,15 +12,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="sr">
       <body className="bg-gray-950 text-white font-sans min-h-screen flex flex-col">
-        {/* Glavna navigacija */}
         <Navbar />
-
-        {/* Sadržaj stranice */}
         <main className="flex-grow container max-w-6xl mx-auto px-4 py-10">
           {children}
         </main>
-
-        {/* Footer */}
         <Footer />
       </body>
     </html>
