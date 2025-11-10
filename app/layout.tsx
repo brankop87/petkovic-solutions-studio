c331ffb2181911da301310028a229f1531107331
brankop87 <51298519+brankop87@users.noreply.github.com>
2025-11-10 23:38:25 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="sr">
       <body className="min-h-screen flex flex-col bg-[#F9EBD0] text-gray-900 font-[Poppins]">
+        {/* Fiksiran Navbar + razmak ispod */}
         <Navbar />
-        <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-8">
           {children}
         </main>
         <Footer />
