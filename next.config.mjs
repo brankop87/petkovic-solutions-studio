6bac7024cdbb21e6cb7d3b26406eca86eef9818e
brankop87 <51298519+brankop87@users.noreply.github.com>
2025-11-14 14:15:15 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // ✅ Optimizacija slika (kada povežemo Sanity)
-  images: {
-    domains: ["cdn.sanity.io"], // Sanity koristi ovaj CDN za slike
-    formats: ["image/avif", "image/webp"],
-  },
-
-  // ✅ Uklanja console.log u produkciji
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-
-  // ✅ Ignoriše ESLint greške prilikom build-a
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // ✅ Sprečava da build puca ako neki tip fali
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'standalone',
 };
 
 export default nextConfig;
