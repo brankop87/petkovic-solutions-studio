cfed142584461d0ceec75ab6b9a73a05fd04f38f
Branko Petković <branko.dvlp@gmail.com>
2026-05-01 21:06:26 +0200

    Add Bella Spa client preview page

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/preview/bella-spa',
+        destination: '/previews/bella-spa/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
